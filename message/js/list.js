e988ec1bcd5d6652446d9b4b1c1db9b35e3c22ca
gq <guanqinghz@163.com>
2019-08-13 15:48:56 +0800

    picker 修复

--- message/js/list.js
+++ message/js/list.js
@@ -14,7 +14,7 @@ require([
             'picker':picker
         },
         template: `<div>
-                      <text-area v-for="zd in zds" :zd="zd"></text-area>
+                      <text-area v-for="zd in zds" :zd="zd" :key="zd.field"></text-area>
                       <picker id="showUserPicker" title="直管领导" @click="getZgld()" :record="zgld"></picker>
                    </div>`,
         data() {
@@ -32,51 +32,53 @@ require([
                 })
             },
             getZgld() {
-                console.log('222222222222222222')
                 var self = this;
                 (function ($,doc) {
-                    var userPicker = new $.PopPicker();
-                    userPicker.setData([{
-                        value: 'ywj',
-                        text: '董事长 叶文洁'
-                    }, {
-                        value: 'aaa',
-                        text: '总经理 艾AA'
-                    }, {
-                        value: 'lj',
-                        text: '罗辑'
-                    }, {
-                        value: 'ymt',
-                        text: '云天明'
-                    }, {
-                        value: 'shq',
-                        text: '史强'
-                    }, {
-                        value: 'zhbh',
-                        text: '章北海'
-                    }, {
-                        value: 'zhy',
-                        text: '庄颜'
-                    }, {
-                        value: 'gyf',
-                        text: '关一帆'
-                    }, {
-                        value: 'zhz',
-                        text: '智子'
-                    }, {
-                        value: 'gezh',
-                        text: '歌者'
-                    }]);
-                    var showUserPickerButton = doc.getElementById('showUserPicker');
-                    //var userResult = doc.getElementById('userResult');
-                    showUserPickerButton.addEventListener('tap', function (event) {
-                        userPicker.show(function (items) {
-                            //userResult.innerText = JSON.stringify(items[0]);
-                            self.zgld = items[0];
-                            //返回 false 可以阻止选择框的关闭
-                            //return false;
-                        });
-                    }, false);
+                    $.init();
+                    $.ready(function () {
+                        var userPicker = new $.PopPicker();
+                        userPicker.setData([{
+                            value: 'ywj',
+                            text: '董事长 叶文洁'
+                        }, {
+                            value: 'aaa',
+                            text: '总经理 艾AA'
+                        }, {
+                            value: 'lj',
+                            text: '罗辑'
+                        }, {
+                            value: 'ymt',
+                            text: '云天明'
+                        }, {
+                            value: 'shq',
+                            text: '史强'
+                        }, {
+                            value: 'zhbh',
+                            text: '章北海'
+                        }, {
+                            value: 'zhy',
+                            text: '庄颜'
+                        }, {
+                            value: 'gyf',
+                            text: '关一帆'
+                        }, {
+                            value: 'zhz',
+                            text: '智子'
+                        }, {
+                            value: 'gezh',
+                            text: '歌者'
+                        }]);
+                        var showUserPickerButton = doc.getElementById('showUserPicker');
+                        //var userResult = doc.getElementById('userResult');
+                        showUserPickerButton.addEventListener('tap', function (event) {
+                            userPicker.show(function (items) {
+                                //userResult.innerText = JSON.stringify(items[0]);
+                                self.zgld = items[0];
+                                //返回 false 可以阻止选择框的关闭
+                                //return false;
+                            });
+                        }, false);
+                    });
                 })(mui,document);
             }
         },
@@ -86,7 +88,6 @@ require([
         mounted(){
             /*this.getZgld();*/
             this.$nextTick(function () {
-                console.log('1111111111111111111111')
                 this.getZgld();
             })
         }
