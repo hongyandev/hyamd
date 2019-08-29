d7c12d0179ab4cc44671dd205c662ecb1b8064f9
Chai <chai@fy.com>
2019-08-29 10:32:26 +0800

    hyamd

--- message/js/list_dj.js
+++ message/js/list_dj.js
@@ -12,7 +12,7 @@ require(['vue', 'components/textArea', 'components/picker','components/navBar','
                            <input type="hidden" id="zblx"/> 
                            <text-area v-for="zd in zds" :isReadOnly="true" :zd="zd" :val="zd.value" ></text-area>
                       </div>
-                      <nav-bar :btnName="btnData.btnname" @btnclick="btnFun"></nav-bar>  
+                      <nav-bar :btnName="buttons" @btnclick="btnFun"></nav-bar>  
                    </div>`,
         data:{
               zds:[],
@@ -24,10 +24,19 @@ require(['vue', 'components/textArea', 'components/picker','components/navBar','
                   },
                   zgldData:[]
               },
-            btnData:{
-                  btnname:[{text:'点检',edit:true}],
-            },
-            xh:getQueryVariable('xh') ? getQueryVariable('xh') : ""
+            xh:getQueryVariable('xh') ? getQueryVariable('xh') : "",
+            state:'2'
+        },
+        computed: {
+            buttons:function () {
+                if(this.state=="2"){
+                    return [{text:'点检', edit:true}];
+                }else if(this.state=="4") {
+                    return [{text:'点检', edit:false}];
+                }else{
+                    return [{text:'点检', edit:true}];
+                }
+            }
         },
         methods: {
             zgldGetData(res){
@@ -79,8 +88,10 @@ require(['vue', 'components/textArea', 'components/picker','components/navBar','
                         self.zds.forEach(function (item) {
                             item.value = perms.detial[item.field];
                         });
+                        self.state=perms.detial['state'];
                     self.zds.filter(val => val.field === 'zblx')[0].value = perms.detial['n_zblx'];
                     $("#wcqk,#py,#jjfa").removeAttr('readonly');
+
                 });
             })
         }
